dddb8ba4ff3d0da582cae0e92b327786ed272a2f
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-07-12 18:48:52 +0530

    ) point bug updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ setInterval(() => {
   let hours= date.getHours()<10 ? `0${date.getHours()}`:date.getHours();
   let min= date.getMinutes()<10 ? `0${date.getMinutes()}`:date.getMinutes();
 
-  let sec=date.getSeconds()<10?`)${date.getSeconds()}`:date.getSeconds();
+  let sec=date.getSeconds()<10?`0${date.getSeconds()}`:date.getSeconds();
   showdate.value=`${hours}:${min}:${sec}`;
  
   
